5485e578eadd4a0aa75b73edae1e85fb982c3d19
Michell0508 <michellgonzaez76@gmail.com>
2024-11-23 22:13:43 -0600

    Pagina principal

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Obtén el botón de hamburguesa y el menú de navegación
+
 const menuToggle = document.getElementById('menu-toggle');
 const navMenu = document.querySelector('nav');
 
-// Escucha el clic en el botón hamburguesa
 menuToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
